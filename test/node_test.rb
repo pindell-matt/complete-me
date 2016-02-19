6cb22d158835ed46360f0d4950b2317df3a113b1
Matt Pindell <pindell.m@gmail.com>
2016-02-19 10:02:12 -0700

    updated node tests

--- test/node_test.rb
+++ test/node_test.rb
@@ -10,15 +10,23 @@ class NodeTest < Minitest::Test
   end
 
   def test_node_initializes_with_word_as_false
-    submitted = @node.word
-    
+    submitted = @node.is_word?
+
     assert_equal false, submitted
   end
 
-  def test_node_initializes_with_children_as_nil
+  def test_node_initializes_with_children_as_empty_hash
+    expected = {}
     submitted = @node.children
 
-    assert_equal nil, submitted
+    assert_equal expected, submitted
+  end
+
+  def test_node_initializes_with_weights_as_empty_hash
+    expected = {}
+    submitted = @node.weights
+
+    assert_equal expected, submitted
   end
 
 end
